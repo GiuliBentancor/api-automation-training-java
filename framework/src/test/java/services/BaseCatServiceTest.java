6cbc1c7598b1df61cbd5033d41a1e34d3cd7fb07
Alansitopay <derpidark@outlook.com>
2025-06-06 22:30:21 -0300

    Arreglo en la lista de temperamento

--- framework/src/test/java/services/BaseCatServiceTest.java
+++ framework/src/test/java/services/BaseCatServiceTest.java
@@ -6,6 +6,9 @@ import models.services.BookingService;
 import models.services.CatService;
 import org.junit.jupiter.api.BeforeEach;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class BaseCatServiceTest {
     protected final CatService service = new CatService();
 
@@ -16,13 +19,13 @@ public class BaseCatServiceTest {
 
     protected CatModel defaultCat() {
         CatModel model = new CatModel();
-        model.setFirstname("Gato1");
+        model.setName("Gato");
         model.setAge(2);
         model.setBreed("Siames");
         model.setDateJoined("2025-06-06T21:27:48.910Z");
         model.setVaccinated(true);
-        model.setTemperament("Friendly");
-        model.setStaffInCharge("Ricardo");
+        model.setTemperament(new ArrayList<>(Arrays.asList("Calm")));
+        model.setStaffInCharge("00000000-0000-0000-0000-000000000000");
         model.setAdopted(true);
         model.setAdopterId(1);
         return model;
