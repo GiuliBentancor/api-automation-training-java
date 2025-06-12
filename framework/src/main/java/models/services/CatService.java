4d26ca6d93f654c19874612535e8edf4dfda4272
Alansitopay <derpidark@outlook.com>
2025-06-11 21:41:25 -0300

    Added CatService methods (patch,delete)

--- framework/src/main/java/models/services/CatService.java
+++ framework/src/main/java/models/services/CatService.java
@@ -13,20 +13,32 @@ public class CatService extends ServiceBase {
         super("/api/cats");
     }
 
+    //POST
     public ResponseContainer<CatResponse> addCats(CatModel model, Map<String, String> headers) {
         return this.post(this.url, model, headers, CatResponse.class);
     }
-
-    /*public ResponseContainer<CatModel> getCatById(Long catId, Map<String, String> headers) {
+    //GET-ID
+    public ResponseContainer<CatModel> getCatById(Long catId, Map<String, String> headers) {
         return this.getSingleObject(this.url + "/" + catId, headers, CatModel.class);
-    }*/
-
+    }
+    //GET
     public ResponseContainer<List<CatModel>> getCats(Map<String, String> headers) {
         return this.getListOfObjects(this.url, headers);
     }
-
+    //PUT-ID
     public ResponseContainer<CatModel> updateCat(Long catId, CatModel model, Map<String, String> headers) {
         String updateUrl = this.url + "/" + catId;
         return this.put(updateUrl, model, headers, CatModel.class);
+    }
+    //DELETE-ID
+    public ResponseContainer<Void> deleteCatById(Long catId, Map<String, String> headers) {
+        String deleteUrl = this.url + "/" + catId;
+        return this.delete(deleteUrl, null, headers, Void.class);
+    }
+
+    // PATCH-ID
+    public ResponseContainer<CatModel> patchCatById(Long catId, CatModel partialUpdate, Map<String, String> headers) {
+        String patchUrl = this.url + "/" + catId;
+        return this.patch(patchUrl, partialUpdate, headers, CatModel.class);
     }
 }
