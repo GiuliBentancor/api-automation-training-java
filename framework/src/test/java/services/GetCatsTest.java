13bde39eec90f0d7004768a37cd9f5e57c3be6ad
Alansitopay <derpidark@outlook.com>
2025-06-13 19:52:25 -0300

    Clase GetCatsTest creada y funcionando

--- framework/src/test/java/services/GetCatsTest.java
+++ framework/src/test/java/services/GetCatsTest.java
@@ -1,14 +1,13 @@
 package services;
 
 import models.requests.CatModel;
-import models.responses.CatResponse;
 import models.responses.ResponseContainer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
+
 import java.util.List;
-import java.util.Map;
+
 
 public class GetCatsTest extends BaseCatServiceTest{
 
