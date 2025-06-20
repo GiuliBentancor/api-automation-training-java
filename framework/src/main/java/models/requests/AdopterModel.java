de95f12bd69e287a07988c7c47bd618c3b4fbde1
Alansitopay <derpidark@outlook.com>
2025-06-20 19:31:08 -0300

    Clase adopter model variables y getters and setters

--- framework/src/main/java/models/requests/AdopterModel.java
+++ framework/src/main/java/models/requests/AdopterModel.java
@@ -1,4 +1,58 @@
 package models.requests;
 
 public class AdopterModel {
+    private Integer id;
+    private String name;
+    private String lastName;
+    private Integer age;
+    private String dateJoined;
+    private String role;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getDateJoined() {
+        return dateJoined;
+    }
+
+    public void setDateJoined(String dateJoined) {
+        this.dateJoined = dateJoined;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
